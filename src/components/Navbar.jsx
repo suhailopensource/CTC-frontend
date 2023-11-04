7c44f947b8ee6cfff51eee6c49aa211d5290fd27
Suhail <suhailcollegemail@gmail.com>
2023-11-04 20:20:44 +0530

    final push and commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const handleClick = () => {
+    setNavbar(!navbar);
+  };
   return (
     <nav className="bg-black border-gray-200 z-50">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-5">
@@ -20,11 +25,9 @@ const Navbar = () => {
             REGISTER
           </button>
           <button
-            data-collapse-toggle="navbar-cta"
             type="button"
+            onClick={handleClick}
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-cta"
-            aria-expanded="false"
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -44,6 +47,49 @@ const Navbar = () => {
             </svg>
           </button>
         </div>
+        {navbar && (
+          <div
+            className="items-center justify-between  w-full "
+            id="navbar-cta"
+          >
+            <ul className="flex flex-col font-medium p-4  mt-4 rounded-lg ">
+              <li onClick={handleClick}>
+                <Link
+                  to="/"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li onClick={handleClick}>
+                <Link
+                  to="/events"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Events
+                </Link>
+              </li>
+              <li onClick={handleClick}>
+                <Link
+                  to="/team"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Team
+                </Link>
+              </li>
+              <li onClick={handleClick}>
+                <Link
+                  to="/blog"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+
         <div
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-cta"
